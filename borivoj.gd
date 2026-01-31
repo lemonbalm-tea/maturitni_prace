c8073b213e0f3868a2baf91f611c1d6e60843ab4
lemonbalm-tea <elliehanakova@gmail.com>
2026-02-01 00:38:45 +0100

    4 - pomoc (31/1)
    existují kolize (ale pěkně blbě) na mezerník je namapovanej spawn pulsu, ale puls je divnej s bílou uprostřed a nekorigujou ho zdi a jsem velmi lost a nevím jak všechno funguje. pomoc

--- borivoj.gd
+++ borivoj.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
-@export_range(0.0, 5.0) var SPEED = 3.0
+@export_range(0.0, 5.0) var SPEED = 100
+var echo_scene = preload("res://sonar test.tscn")
+func get_input():
+	var dir = Input.get_vector("left","right","up","down")
+	velocity = dir * SPEED
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		position.x = position.x + SPEED
-	if Input.is_action_pressed("left"):
-		position.x = position.x - SPEED
-	if Input.is_action_pressed("up"):
-		position.y = position.y - SPEED
-	if Input.is_action_pressed("down"):
-		position.y = position.y + SPEED
+	get_input()
+	move_and_slide()
+	if Input.is_action_just_pressed("echo"):
+		instance_object()
+func instance_object():
+
+	var position = $".".global_position
+	
+	var echo = echo_scene.instantiate()
+	get_parent().add_child(echo)
+	echo.position = Vector2 (position.x, position.y)
+	echo.scale = Vector2 (0.2, 0.2)
