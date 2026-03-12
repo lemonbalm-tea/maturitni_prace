9cb9ca0fefd322e66901b1ea7f8e8ca99c3f0d8a
lemonbalm-tea <elliehanakova@gmail.com>
2026-03-12 22:09:07 +0100

    20.1 - Bugfix checkpoint (12/3)
    bug ze kdyz se clovek dostane na checkpoint a pak zmeni level, teleportuje ho to na misto toho checkpointu v jinym levelu

--- matprac-2/scripts/level_select.gd
+++ matprac-2/scripts/level_select.gd
@@ -4,13 +4,14 @@ extends Control
 
 func _on_swamp_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
+	Global.checkpoint = null
 
 
 func _on_forest_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
-
+	Global.checkpoint = null
 func _on_castle_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level3.tscn")
-
+	Global.checkpoint = null
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
