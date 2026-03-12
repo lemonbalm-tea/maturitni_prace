5d0dd49441fa1510ba9e7a5ecb95d6b9c46187e7
lemonbalm-tea <elliehanakova@gmail.com>
2026-03-12 02:18:04 +0100

    19 - Dokončení textu (11/3)
    rozsireno o dalsi assety (zitra budou uz vsechny), pridan esc a back , pozadi levelu 2 a (provizotni) pozadi pro l3. pridana hudba a zkrasleno hlavni menu

--- matprac-2/scripts/imput.gd
+++ matprac-2/scripts/imput.gd
@@ -3,6 +3,10 @@ extends Node
 var input_h = 0.0
 func _process(delta):
 	input_h = Input.get_axis("left","right")
+	if Input.is_action_just_pressed("escape"):
+		get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
+	if Input.is_action_just_pressed("back"):
+		get_tree().call_deferred("reload_current_scene")
 	
 func is_jump():
 	return Input.is_action_just_pressed("jump")
